290631b44bc2729016e2ba82754f88528d515431
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-10-25 19:01:58 +0700

    Create jwt auth

---
+++ backend/internal/auth/jwt/jwt.go
@@ -0,0 +1,44 @@
+package jwt
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte(os.Getenv("JWT_SECRET"))
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJWT(username string) (string, error) {
+	expirationTime := time.Now().Add(20 * time.Minute)
+	claims := &Claims{
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
+
+func ValidateToken(tokenStr string) (*Claims, error) {
+    claims := &Claims{}
+    token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+        return jwtKey, nil
+    })
+    if err != nil || !token.Valid {
+        return nil, err
+    }
+
+    if claims.ExpiresAt.Time.Before(time.Now()) {
+        return nil, fmt.Errorf("token expired")
+    }
+
+    return claims, nil
+}
