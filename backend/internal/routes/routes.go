4b4549c04268c2cb93f8421caa67e76cb501cad6
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-10-26 15:14:47 +0700

    Create route for login

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/AndreanDjabbar/CaysFashion/backend/internal/auth/jwt"
 	"github.com/AndreanDjabbar/CaysFashion/backend/internal/handlers"
 	"github.com/gin-gonic/gin"
 )
@@ -11,13 +12,10 @@ func SetUpMainRoutes(router *gin.Engine) {
 	route := router.Group("/caysfashion")
 	route.GET("/", handlers.MainRootHandler)
 	route.POST("/register", handlers.RegisterHandler)
-}
+	route.POST("/login", handlers.LoginHandler)
 
-// func SetUpSpecialRoutes(router *gin.Engine) {
-// 	store2 := cookie.NewStore([]byte("secret2"))
-// 	store2.Options(sessions.Options{
-// 		// different options
-// 	})
-// 	router.Use(sessions.Sessions("mysession", store2))
-// 	// router.GET("/", ...)
-// }
+	route.Use(jwt.AuthMiddleware()) 
+	{
+		route.GET("/home", handlers.HomeHandler)
+	}
+}
