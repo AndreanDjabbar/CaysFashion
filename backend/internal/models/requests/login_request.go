e355f30341f1d6f03ff8ebd1152a5c5094864f81
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-10-26 15:16:23 +0700

    Create login request model

--- backend/internal/models/requests/login_request.go
+++ backend/internal/models/requests/login_request.go
@@ -1,7 +1,6 @@
 package requests
 
-type RegisterRequest struct {
+type LoginRequest struct {
     Username string `json:"username"`
-    Email    string `json:"email"`
     Password string `json:"password"`
 }
